79d0bbd18ba6eab52228dda4d6b06ac0ecacb4b5
MuhaDev <muhammadillohalimov6@gmail.com>
2025-05-18 15:20:51 +0500

    ssss

---
+++ OOP.java
@@ -0,0 +1,13 @@
+public class OOP {
+    public static void main(String[] args) {
+        int a = 5;
+        int b =3;
+
+        if(a>b){
+            System.out.println("a katta");
+        }
+    }
+
+
+
+}
