0cd0ddbdffce4f7ae29d1dec59f716b759e9c1d1
MuhaDev <muhammadillohalimov6@gmail.com>
2025-05-18 15:33:44 +0500

    update

---
+++ src/lessonJavaCore/ArifmetikAmallar.java
@@ -0,0 +1,13 @@
+package lessonJavaCore;
+
+public class ArifmetikAmallar {
+    public static void main(String[] args) {
+        int a=15;
+        int b=5;
+        System.out.println("qoshish: "+(a+b));
+        System.out.println("Ayirish: "+(a-b));
+        System.out.println("Kopaytirish: "+(a*b));
+        System.out.println("Bolish: "+(a/b));
+        System.out.println("Qoldiq: "+(a % b));
+    }
+}
---
+++ src/lessonJavaCore/Car.java
@@ -0,0 +1,19 @@
+package lessonJavaCore;
+
+public class Car {
+    // modeli
+    // color
+    public void getColor(String color){
+        System.out.println(color);
+    }
+    public void getModel(String model){
+        System.out.println(model);
+    }
+    public void getPrice(String price){
+        System.out.println(price);
+    }
+    public void getAutomatic(String automotic){
+        System.out.println(automotic);
+    }
+
+}
